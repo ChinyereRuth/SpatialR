b995ea88c754d3f2ff093c1849b33a443a6b2eeb
Chinyere Ottah <ottahc@mcmaster.ca>
2023-11-01 06:25:12 -0400

    Initial commit

---
+++ R/Spatial.R
@@ -0,0 +1,10 @@
+# Initialize the description of the package; edit the file with all relevant details.
+
+usethis::use_description()
+
+# Initialize a file to document the package; this file will be created in the folder `/R`.
+usethis::use_package_doc()
+
+# Initialize a license; check other alternatives like `usethis::use_ccby_license()`.
+usethis::use_mit_license()
+
---
+++ R/SpatialR-package.R
@@ -0,0 +1,22 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
+
+#' SpatialR: A package with templates and data that is importantt for carrying oput surface anaysis using DEM using `R
+#'
+#' #' @docType package
+#'
+#' #' @author Chinyere Ottah, School of Earth, Environment and Society, McMaster University \email{ottahc@@mcmaster.ca}
+#' @references \url{https://github.com//ChinyereRuth/Spat}
+#'
+##############################################
+##                DATA SETS                 ##
+##############################################
+
+## Elevation
+
+#' @Tiff data
+#' @keywords datasets
